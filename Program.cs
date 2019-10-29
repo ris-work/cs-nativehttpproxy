6fd834078ec4aae013d75dc33c4362c6cc8f4f00
RishiKL <52776758+ris-public@users.noreply.github.com>
2019-10-29 18:11:50 +0530

    Implement MDN explanation for HTTP CONNECT.

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Text;
+using System.Net.Http;
+using System.IO;
+using System.Net.Sockets;
+
+namespace cs_nativehttpproxy
+{
+	public class HTTPProxy
+	{
+		TcpClient Client=new TcpClient();
+		string Username;
+		string Pass;
+		string Target=null;
+		bool UseAuth=false;
+		string Proxy;
+		int ProxyPort;
+		HTTPProxy (string Target, string Proxy, int ProxyPort)
+		{
+			this.Target=Target;
+			this.Proxy=Proxy;
+			this.ProxyPort=ProxyPort;
+		}
+		HTTPProxy (string Target, string Proxy, int ProxyPort, string Username, string Password)
+		{
+			this.Target=Target;
+			this.Proxy=Proxy;
+			this.ProxyPort=ProxyPort;
+			this.Username=Username;
+			this.Pass=Password;
+		}
+		Stream GetStream()
+		{
+			Client.Connect(Proxy, ProxyPort);
+			Stream S = Client.GetStream();
+			byte[] Buffer=new byte[]{};
+			string AuthStr64=Convert.ToBase64String(Encoding.UTF8.GetBytes($"{Username}:{Pass}"));
+			if(UseAuth==false) 
+				Buffer = Encoding.UTF8.GetBytes($"CONNECT {Target} HTTP/1.1\r\n");
+else
+				Buffer = Encoding.UTF8.GetBytes($"CONNECT {Target} HTTP/1.1\r\nHost: {Target}\r\nProxy-Authorization: {AuthStr64}\r\n");
+			S.Write(Buffer);
+			return S;
+		}
+	}
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}	
