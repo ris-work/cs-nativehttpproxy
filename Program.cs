869838f21d206a7af2b5a7f9c929fbac9c3df810
RishiKL <52776758+ris-public@users.noreply.github.com>
2019-10-29 22:05:25 +0530

    gui.cs WIP.

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Text;
 using System.Net.Http;
 using System.IO;
 using System.Net.Sockets;
+using Terminal.Gui;
 
 namespace Rishi.ProxyClient
 {
@@ -15,13 +16,13 @@ namespace Rishi.ProxyClient
 		bool UseAuth=false;
 		string Proxy;
 		int ProxyPort;
-		HTTPProxy (string Target, string Proxy, int ProxyPort)
+		HTTPProxyClient (string Target, string Proxy, int ProxyPort)
 		{
 			this.Target=Target;
 			this.Proxy=Proxy;
 			this.ProxyPort=ProxyPort;
 		}
-		HTTPProxy (string Target, string Proxy, int ProxyPort, string Username, string Password)
+		HTTPProxyClient (string Target, string Proxy, int ProxyPort, string Username, string Password)
 		{
 			this.Target=Target;
 			this.Proxy=Proxy;
@@ -50,6 +51,52 @@ namespace Rishi.ProxyClient
 	{
 		static void Main(string[] args)
 		{
+			Application.Init ();
+			var top = Application.Top;
+
+			// Creates the top-level window to show
+			var win = new Window ("MyApp") {
+				X = 0,
+				  Y = 1, // Leave one row for the toplevel menu
+
+				  // By using Dim.Fill(), it will automatically resize without manual intervention
+				  Width = Dim.Fill (),
+				  Height = Dim.Fill ()
+			};
+			top.Add (win);
+			var proxy = new Label ("Proxy: ") { X = 3, Y = 2 };
+			var target = new Label ("Target: ") {
+				X = Pos.Left (login),
+				  Y = Pos.Top (login) + 1
+			};
+			var proxyText = new TextField ("") {
+				X = Pos.Right (password),
+				  Y = Pos.Top (login),
+				  Width = 40
+			};
+			var targetText = new TextField ("") {
+				Secret = true,
+					   X = Pos.Left (loginText),
+					   Y = Pos.Top (password),
+					   Width = Dim.Width (loginText)
+			};
+			var login = new Label ("Login: ") { X = 3, Y = 2 };
+			var password = new Label ("Password: ") {
+				X = Pos.Left (login),
+				  Y = Pos.Top (login) + 1
+			};
+			var loginText = new TextField ("") {
+				X = Pos.Right (password),
+				  Y = Pos.Top (login),
+				  Width = 40
+			};
+			var passText = new TextField ("") {
+				Secret = true,
+					   X = Pos.Left (loginText),
+					   Y = Pos.Top (password),
+					   Width = Dim.Width (loginText)
+			};
+			Application.Run ();
 			Console.WriteLine("Hello World!");
 		}
 	}
